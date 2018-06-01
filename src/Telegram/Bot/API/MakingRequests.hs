f105bb9053d8a66847e639954cbdeb782a69f24f
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-06-01 08:44:42 -0600

    Add IsString instance for Token

--- src/Telegram/Bot/API/MakingRequests.hs
+++ src/Telegram/Bot/API/MakingRequests.hs
@@ -1,20 +1,22 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings          #-}
 module Telegram.Bot.API.MakingRequests where
 
-import Data.Aeson (ToJSON(..), FromJSON(..))
-import Data.Monoid ((<>))
-import Data.Text (Text)
-import qualified Data.Text as Text
-import GHC.Generics (Generic)
-import Servant.Client hiding (Response)
-import Web.HttpApiData (ToHttpApiData(..), FromHttpApiData)
-import Network.HTTP.Client (newManager)
-import Network.HTTP.Client.TLS (tlsManagerSettings)
+import           Data.Aeson                      (FromJSON (..), ToJSON (..))
+import           Data.Monoid                     ((<>))
+import           Data.String                     (IsString)
+import           Data.Text                       (Text)
+import qualified Data.Text                       as Text
+import           GHC.Generics                    (Generic)
+import           Network.HTTP.Client             (newManager)
+import           Network.HTTP.Client.TLS         (tlsManagerSettings)
+import           Servant.Client                  hiding (Response)
+import           Web.HttpApiData                 (FromHttpApiData,
+                                                  ToHttpApiData (..))
 
-import Telegram.Bot.API.Internal.Utils
-import Telegram.Bot.API.Types
+import           Telegram.Bot.API.Internal.Utils
+import           Telegram.Bot.API.Types
 
 botBaseUrl :: Token -> BaseUrl
 botBaseUrl token = BaseUrl Https "api.telegram.org" 443
@@ -43,4 +45,4 @@ instance ToJSON   a => ToJSON   (Response a) where toJSON = gtoJSON
 instance FromJSON a => FromJSON (Response a) where parseJSON = gparseJSON
 
 newtype Token = Token Text
-  deriving (Eq, Show, ToHttpApiData, FromHttpApiData, ToJSON, FromJSON)
+  deriving (Eq, Show, ToHttpApiData, FromHttpApiData, ToJSON, FromJSON, IsString)
