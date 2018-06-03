ce7d1f741b91842dce9da68d945932d252eb44be
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-06-03 11:23:25 -0600

    Add getEnvToken helper

--- src/Telegram/Bot/Simple/BotApp.hs
+++ src/Telegram/Bot/Simple/BotApp.hs
@@ -4,16 +4,21 @@ module Telegram.Bot.Simple.BotApp where
 
 import           Control.Concurrent          (forkIO, threadDelay)
 import           Control.Concurrent.STM
-import           Control.Monad.Except
-import           Control.Monad.Trans.Control
-import           Servant.Client
+import           Control.Monad               (void)
+import           Control.Monad.Except        (catchError)
+import           Control.Monad.Trans         (liftIO)
+import           Control.Monad.Trans.Control (liftBaseDiscard)
+import           Data.String                 (fromString)
+import           Servant.Client              (ClientEnv, ClientM, ServantError,
+                                              runClientM)
+import           System.Environment          (getEnv)
 
-import           Telegram.Bot.API
+import qualified Telegram.Bot.API            as Telegram
 import           Telegram.Bot.Simple.Eff
 
 data BotApp model action = BotApp
   { botInitialModel :: model
-  , botAction       :: Update -> model -> Maybe action
+  , botAction       :: Telegram.Update -> model -> Maybe action
   , botHandler      :: action -> model -> Eff action model
   , botJobs         :: [BotJob model action]
   }
@@ -60,15 +65,15 @@ startBotPolling BotApp{..} = startPolling . handleUpdate
           Nothing -> return []
       mapM_ ((>>= handleAction' modelVar update . const . Just) . runBotM update) actions
 
-startPolling :: (Update -> ClientM ()) -> ClientM ()
+startPolling :: (Telegram.Update -> ClientM ()) -> ClientM ()
 startPolling handleUpdate = go Nothing
   where
     go lastUpdateId = do
-      let inc (UpdateId n) = UpdateId (n + 1)
+      let inc (Telegram.UpdateId n) = Telegram.UpdateId (n + 1)
           offset = fmap inc lastUpdateId
       res <-
-        (Right <$> getUpdates
-          (GetUpdatesRequest offset Nothing Nothing Nothing))
+        (Right <$> Telegram.getUpdates
+          (Telegram.GetUpdatesRequest offset Nothing Nothing Nothing))
         `catchError` (pure . Left)
 
       nextUpdateId <- case res of
@@ -76,11 +81,20 @@ startPolling handleUpdate = go Nothing
           liftIO (print servantErr)
           pure lastUpdateId
         Right result -> do
-          let updates = responseResult result
-              updateIds = map updateUpdateId updates
+          let updates = Telegram.responseResult result
+              updateIds = map Telegram.updateUpdateId updates
               maxUpdateId = maximum (Nothing : map Just updateIds)
           mapM_ handleUpdate updates
           pure maxUpdateId
       liftIO $ threadDelay 1000000
       go nextUpdateId
 
+-- | Get a 'Telegram.Token' from environment variable.
+--
+-- Common use:
+--
+-- @
+-- 'getEnvToken' "TELEGRAM_BOT_TOKEN"
+-- @
+getEnvToken :: String -> IO Telegram.Token
+getEnvToken varName = fromString <$> getEnv varName
