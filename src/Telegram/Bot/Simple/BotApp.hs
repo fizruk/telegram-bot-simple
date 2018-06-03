98c869ae2c66a1bffdf603b539cc99703a2bbcc0
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-06-03 17:03:25 -0600

    Introduce BotEnv with model state and action queue

--- src/Telegram/Bot/Simple/BotApp.hs
+++ src/Telegram/Bot/Simple/BotApp.hs
@@ -3,21 +3,20 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Telegram.Bot.Simple.BotApp where
 
-import           Control.Concurrent          (ThreadId, forkIO, threadDelay)
+import           Control.Concurrent      (ThreadId, forkIO, threadDelay)
 import           Control.Concurrent.STM
-import           Control.Monad               (void)
-import           Control.Monad.Except        (catchError)
-import           Control.Monad.Trans         (liftIO)
-import           Control.Monad.Trans.Control (liftBaseDiscard)
-import           Data.Bifunctor              (first)
-import           Data.String                 (fromString)
-import           Data.Text                   (Text)
-import           Servant.Client              (ClientEnv, ClientM, ServantError,
-                                              runClientM)
-import qualified System.Cron                 as Cron
-import           System.Environment          (getEnv)
+import           Control.Monad           (forever, void)
+import           Control.Monad.Except    (catchError)
+import           Control.Monad.Trans     (liftIO)
+import           Data.Bifunctor          (first)
+import           Data.String             (fromString)
+import           Data.Text               (Text)
+import           Servant.Client          (ClientEnv, ClientM, ServantError,
+                                          runClientM)
+import qualified System.Cron             as Cron
+import           System.Environment      (getEnv)
 
-import qualified Telegram.Bot.API            as Telegram
+import qualified Telegram.Bot.API        as Telegram
 import           Telegram.Bot.Simple.Eff
 
 data BotApp model action = BotApp
@@ -32,36 +31,46 @@ data BotJob model action = BotJob
   , botJobTask     :: model -> Eff action model  -- ^ Job function.
   }
 
+data BotEnv model action = BotEnv
+  { botModelVar     :: TVar model
+  , botActionsQueue :: TQueue (Maybe Telegram.Update, action)
+  }
+
 instance Functor (BotJob model) where
   fmap f BotJob{..} = BotJob{ botJobTask = first f . botJobTask, .. }
 
-runJobTask :: TVar model -> ClientEnv -> (model -> Eff action model) -> IO ()
-runJobTask modelVar env task = do
-  actions <- liftIO $ atomically $ do
-    model <- readTVar modelVar
+runJobTask :: BotEnv model action -> ClientEnv -> (model -> Eff action model) -> IO ()
+runJobTask botEnv@BotEnv{..} env task = do
+  effects <- liftIO $ atomically $ do
+    model <- readTVar botModelVar
     case runEff (task model) of
-      (newModel, actions) -> do
-        writeTVar modelVar newModel
-        return actions
+      (newModel, effects) -> do
+        writeTVar botModelVar newModel
+        return effects
   res <- flip runClientM env $
-    mapM_ (runBotM Nothing) actions -- TODO: handle issued actions
+    mapM_ ((>>= liftIO . issueAction botEnv Nothing) . runBotM Nothing) effects
   case res of
-    Left err     -> print err
-    Right result -> return ()
+    Left err -> print err
+    Right _  -> return ()
 
-scheduleBotJob :: TVar model -> ClientEnv -> BotJob model action -> IO [ThreadId]
-scheduleBotJob modelVar env BotJob{..} = Cron.execSchedule $ do
-  Cron.addJob (runJobTask modelVar env botJobTask) botJobSchedule
+scheduleBotJob :: BotEnv model action -> ClientEnv -> BotJob model action -> IO [ThreadId]
+scheduleBotJob botEnv env BotJob{..} = Cron.execSchedule $ do
+  Cron.addJob (runJobTask botEnv env botJobTask) botJobSchedule
 
-scheduleBotJobs :: TVar model -> ClientEnv -> [BotJob model action] -> IO [ThreadId]
-scheduleBotJobs modelVar env jobs = concat
-  <$> traverse (scheduleBotJob modelVar env) jobs
+scheduleBotJobs :: BotEnv model action -> ClientEnv -> [BotJob model action] -> IO [ThreadId]
+scheduleBotJobs botEnv env jobs = concat
+  <$> traverse (scheduleBotJob botEnv env) jobs
+
+defaultBotEnv :: BotApp model action -> IO (BotEnv model action)
+defaultBotEnv BotApp{..} = BotEnv
+  <$> newTVarIO botInitialModel
+  <*> newTQueueIO
 
 startBotAsync :: BotApp model action -> ClientEnv -> IO (action -> IO ())
 startBotAsync bot env = do
-  modelVar <- newTVarIO (botInitialModel bot)
-  jobThreadIds <- scheduleBotJobs modelVar env (botJobs bot)
-  fork_ $ startBotPolling bot modelVar
+  botEnv <- defaultBotEnv bot
+  jobThreadIds <- scheduleBotJobs botEnv env (botJobs bot)
+  fork_ $ startBotPolling bot botEnv
   return undefined
   where
     fork_ = void . forkIO . void . flip runClientM env
@@ -71,30 +80,50 @@ startBotAsync_ bot env = void (startBotAsync bot env)
 
 startBot :: BotApp model action -> ClientEnv -> IO (Either ServantError ())
 startBot bot env = do
-  modelVar <- newTVarIO (botInitialModel bot)
-  jobThreadIds <- scheduleBotJobs modelVar env (botJobs bot)
-  runClientM (startBotPolling bot modelVar) env
-
+  botEnv <- defaultBotEnv bot
+  jobThreadIds <- scheduleBotJobs botEnv env (botJobs bot)
+  _actionsThreadId <- processActionsIndefinitely bot botEnv env
+  runClientM (startBotPolling bot botEnv) env
 startBot_ :: BotApp model action -> ClientEnv -> IO ()
 startBot_ bot = void . startBot bot
 
-startBotPolling :: BotApp model action -> TVar model -> ClientM ()
-startBotPolling BotApp{..} = startPolling . handleUpdate
-  where
-    handleUpdate modelVar update = void . liftBaseDiscard forkIO $
-      handleAction' modelVar (Just update) (botAction update)
-      `catchError` (liftIO . print) -- print error on failed update handlers
+issueAction :: BotEnv model action -> Maybe Telegram.Update -> action -> IO ()
+issueAction BotEnv{..} update action = atomically $
+  writeTQueue botActionsQueue (update, action)
 
-    handleAction' modelVar update toAction = do
-      actions <- liftIO $ atomically $ do
-        model <- readTVar modelVar
-        case toAction model of
-          Just action -> case runEff (botHandler action model) of
-            (newModel, actions) -> do
-              writeTVar modelVar newModel
-              return actions
-          Nothing -> return []
-      mapM_ ((>>= handleAction' modelVar update . const . Just) . runBotM update) actions
+processAction
+  :: BotApp model action
+  -> BotEnv model action
+  -> Maybe Telegram.Update
+  -> action
+  -> ClientM ()
+processAction BotApp{..} botEnv@BotEnv{..} update action = do
+  effects <- liftIO $ atomically $ do
+    model <- readTVar botModelVar
+    case runEff (botHandler action model) of
+      (newModel, effects) -> do
+        writeTVar botModelVar newModel
+        return effects
+  mapM_ ((>>= liftIO . issueAction botEnv update) . runBotM update) effects
+
+processActionJob :: BotApp model action -> BotEnv model action -> ClientM ()
+processActionJob botApp botEnv@BotEnv{..} = do
+  (update, action) <- liftIO . atomically $ readTQueue botActionsQueue
+  processAction botApp botEnv update action
+
+processActionsIndefinitely
+  :: BotApp model action -> BotEnv model action -> ClientEnv -> IO ThreadId
+processActionsIndefinitely botApp botEnv env = forkIO . forever $ do
+  runClientM (processActionJob botApp botEnv) env
+
+startBotPolling :: BotApp model action -> BotEnv model action -> ClientM ()
+startBotPolling BotApp{..} botEnv@BotEnv{..} = startPolling handleUpdate
+  where
+    handleUpdate update = liftIO . void . forkIO $ do
+      maction <- botAction update <$> readTVarIO botModelVar
+      case maction of
+        Nothing     -> return ()
+        Just action -> issueAction botEnv (Just update) action
 
 startPolling :: (Telegram.Update -> ClientM ()) -> ClientM ()
 startPolling handleUpdate = go Nothing
