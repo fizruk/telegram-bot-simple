2fe9a0524864f13e5bd5686d0aaf0c082300d385
Artem Kobzar <a.kobzar.nlt@gmail.com>
2018-11-10 15:55:22 +0200

    feat(Message Method): add DeleteMessage telegram API method.

--- src/Telegram/Bot/API/Methods.hs
+++ src/Telegram/Bot/API/Methods.hs
@@ -5,15 +5,19 @@
 module Telegram.Bot.API.Methods where
 
 import Data.Aeson
+import Data.Coerce (coerce)
 import Data.Proxy
 import Data.Text (Text)
 import GHC.Generics (Generic)
+import GHC.Int (Int32)
 import Servant.API
 import Servant.Client hiding (Response)
 
 import Telegram.Bot.API.Internal.Utils
 import Telegram.Bot.API.MakingRequests
 import Telegram.Bot.API.Types
+
+type RequiredQueryParam = QueryParam' '[Required, Strict]
 
 -- * Available methods
 
@@ -26,6 +30,22 @@ type GetMe = "getMe" :> Get '[JSON] (Response User)
 -- Returns basic information about the bot in form of a 'User' object.
 getMe :: ClientM (Response User)
 getMe = client (Proxy @GetMe)
+
+-- ** 'deleteMessage'
+
+-- | Notice that deleting by POST method was bugged, so we use GET
+type DeleteMessage = "deleteMessage"
+  :> RequiredQueryParam "chat_id" Integer
+  :> RequiredQueryParam "message_id" Int32
+  :> Get '[JSON] (Response Bool)
+
+-- | Use this method to delete message in chat.
+-- On success, the sent Bool is returned.
+deleteMessage :: ChatId -> MessageId -> ClientM (Response Bool)
+deleteMessage chatId messageId = client (Proxy @DeleteMessage) chatId' messageId'
+  where
+    chatId' = coerce chatId
+    messageId' = coerce messageId
 
 -- ** 'sendMessage'
 
