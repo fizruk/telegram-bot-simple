074942bb542effd1e1450a5598939d0530865a13
Andrey Prokopenko <persiantiger@yandex.ru>
2021-12-20 21:16:44 +0100

    Fix `MessageId` overflow, changing to `Integer`

--- src/Telegram/Bot/API/Types.hs
+++ src/Telegram/Bot/API/Types.hs
@@ -165,11 +165,11 @@ data Message = Message
   deriving (Generic, Show)
 
 -- | Unique message identifier inside this chat.
-newtype MessageId = MessageId Int32
+newtype MessageId = MessageId Integer
   deriving (Eq, Show, ToJSON, FromJSON, Hashable)
 
 instance ToHttpApiData MessageId where
-  toUrlPiece a = pack . show @Int32 $ coerce a
+  toUrlPiece a = pack . show @Integer $ coerce a
 
 -- | The unique identifier of a media message group a message belongs to.
 newtype MediaGroupId = MediaGroupId Text
