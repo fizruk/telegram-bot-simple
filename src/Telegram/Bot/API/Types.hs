bf612912ded921814e44a0f20c52d3558ad62349
Andrey Prokopenko <persiantiger@yandex.ru>
2023-02-06 20:23:08 +0100

    Fix `ReplyKeyboardMarkup`

--- src/Telegram/Bot/API/Types.hs
+++ src/Telegram/Bot/API/Types.hs
@@ -588,7 +588,7 @@ instance ToJSON InputFile where
 -- | This object represents a custom keyboard with reply options (see Introduction to bots for details and examples).
 data ReplyKeyboardMarkup = ReplyKeyboardMarkup
   { replyKeyboardMarkupKeyboard           :: [[KeyboardButton]] -- ^ Array of button rows, each represented by an Array of KeyboardButton objects.
-  , replyKeyboaardMarkupIsPersistent      :: Maybe Bool         -- ^ Requests clients to always show the keyboard when the regular keyboard is hidden. Defaults to 'False', in which case the custom keyboard can be hidden and opened with a keyboard icon.
+  , replyKeyboardMarkupIsPersistent      :: Maybe Bool         -- ^ Requests clients to always show the keyboard when the regular keyboard is hidden. Defaults to 'False', in which case the custom keyboard can be hidden and opened with a keyboard icon.
   , replyKeyboardMarkupResizeKeyboard     :: Maybe Bool         -- ^ Requests clients to resize the keyboard vertically for optimal fit (e.g., make the keyboard smaller if there are just two rows of buttons). Defaults to false, in which case the custom keyboard is always of the same height as the app's standard keyboard.
   , replyKeyboardMarkupOneTimeKeyboard    :: Maybe Bool         -- ^ Requests clients to hide the keyboard as soon as it's been used. The keyboard will still be available, but clients will automatically display the usual letter-keyboard in the chat – the user can press a special button in the input field to see the custom keyboard again. Defaults to false.
   , replyKeyboardMarkupInputFieldSelector :: Maybe Text         -- ^ The placeholder to be shown in the input field when the keyboard is active; 1-64 characters.
